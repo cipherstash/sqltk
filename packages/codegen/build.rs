332a3472ba0082db23b54b4f9a789f641cb7469b
James Sadler <james@cipherstash.com>
2024-01-28 13:12:27 +1100

    Put codegen's generated code in 'generated' dir

--- packages/codegen/build.rs
+++ packages/codegen/build.rs
@@ -1,49 +1,30 @@
 use std::path::PathBuf;
 
 mod build_helpers;
-pub use build_helpers::*;
+use build_helpers::*;
 
 fn main() {
     let codegen = Codegen::new();
 
-    let node_list_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_node_list.json");
-
-    codegen.generate_node_list_file(&node_list_file );
-
-    let dispatch_table_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_dispatch_table_trait.rs");
-
-    codegen.generate_dispatch_table_trait(&dispatch_table_file);
-
-    let dispatch_table_lookup_impls_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_dispatch_table_lookup_impls.rs");
-
-    codegen.generate_dispatch_table_lookup_impls(&dispatch_table_lookup_impls_file);
-
-    let dispatch_impls_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_dispatch_impls.rs");
-
-    codegen.generate_dispatch_impls(&dispatch_impls_file);
-
-    let ast_node_impls_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_ast_node_impls.rs");
-
-    let reachability_debug_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("reachability-debug.txt");
-
-    codegen.generate_ast_node_impls(&ast_node_impls_file, &reachability_debug_file);
-
-    let concrete_node_enum_file =
-        PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("generated_concrete_node_enum.rs");
-
-    codegen.generated_concrete_node_enum(&concrete_node_enum_file);
-
-    let concrete_node_enum_match_macro_file = PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("generated_concrete_node_enum_match_macro.rs");
-
-    codegen.generate_concrete_node_enum_match_macro(&concrete_node_enum_match_macro_file);
+    codegen.generate_node_list_file(&output_path("node_list.json"));
+    codegen.generate_dispatch_table_trait(&output_path("dispatch_table_trait.rs"));
+    codegen.generate_dispatch_table_lookup_impls(&output_path("dispatch_table_lookup_impls.rs"));
+    codegen.generate_dispatch_impls(&output_path("dispatch_impls.rs"));
+    codegen.generate_ast_node_impls(
+        &output_path("ast_node_impls.rs"),
+        &output_path("reachability-debug.txt"),
+    );
+    codegen.generated_concrete_node_enum(&output_path("concrete_node_enum.rs"));
+    codegen
+        .generate_concrete_node_enum_match_macro(&output_path("concrete_node_enum_match_macro.rs"));
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:rerun-if-changed=Cargo.lock");
+}
+
+fn output_path(file: &str) -> PathBuf {
+    PathBuf::from(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("generated")
+        .join(file)
 }
--- packages/codegen/src/lib.rs
+++ packages/codegen/src/lib.rs
@@ -1,29 +1,29 @@
 use lazy_static_include::*;
 
 lazy_static_include_str!(
-    pub VISITOR_DISPATCH_TABLE => "generated_dispatch_table_trait.rs"
+    pub VISITOR_DISPATCH_TABLE => "generated/dispatch_table_trait.rs"
 );
 
 lazy_static_include_str!(
-    pub VISITOR_DISPATCH_TABLE_LOOKUP_IMPLS => "generated_dispatch_table_lookup_impls.rs"
+    pub VISITOR_DISPATCH_TABLE_LOOKUP_IMPLS => "generated/dispatch_table_lookup_impls.rs"
 );
 
 lazy_static_include_str!(
-    pub VISITOR_DISPATCH_IMPLS => "generated_dispatch_impls.rs"
+    pub VISITOR_DISPATCH_IMPLS => "generated/dispatch_impls.rs"
 );
 
 lazy_static_include_str!(
-    pub AST_NODE_IMPLS => "generated_ast_node_impls.rs"
+    pub AST_NODE_IMPLS => "generated/ast_node_impls.rs"
 );
 
 lazy_static_include_str!(
-    pub CONCRETE_NODE_ENUM => "generated_concrete_node_enum.rs"
+    pub CONCRETE_NODE_ENUM => "generated/concrete_node_enum.rs"
 );
 
 lazy_static_include_str!(
-    pub CONCRETE_NODE_ENUM_MATCH_MACRO => "generated_concrete_node_enum_match_macro.rs"
+    pub CONCRETE_NODE_ENUM_MATCH_MACRO => "generated/concrete_node_enum_match_macro.rs"
 );
 
 lazy_static_include_str!(
-    pub NODE_LIST => "generated_node_list.json"
+    pub NODE_LIST => "generated/node_list.json"
 );
